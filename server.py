7417cb2c4afe42da761de4850a3180089db927f6
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-07-19 11:01:15 -0500

    3

--- server.py
+++ server.py
@@ -1,3 +1,15 @@
 from flask import Flask, render_template
 from user import Users
 
+@app.route('/users')
+def index():
+    users = users.get_all()
+    print(users)
+    return render_template("read.html", all_users = users)
+
+
+@app.route('/users/new')
+
+@app.route('/users/4')
+
+@app.route('/users/4/edit')
