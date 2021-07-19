888e6f9c4b57fd305334073b2213a45a8935c4cc
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-07-19 14:21:19 -0500

    54

--- server.py
+++ server.py
@@ -9,7 +9,19 @@ def index():
 
 
 @app.route('/users/new')
+def index():
+    users = users.get_all()
+    print(users)
+    return render_template("create.html", all_users = users)
 
 @app.route('/users/4')
+def index():
+    users = users.get_all()
+    print(users)
+    return render_template("read.html", all_users = users)
 
 @app.route('/users/4/edit')
+def index():
+    users = users.get_all()
+    print(users)
+    return render_template("read.html", all_users = users)
